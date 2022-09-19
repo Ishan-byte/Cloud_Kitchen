192be0b2fdaef1e7e7079ef1b1da286f4daa4bbb
Ishan Chemjong <limbuisan@gmail.com>
2022-09-19 22:01:07 +0545

    Complete Project Structure and Navbar Completion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
+// Imports
+import { RouterModule, Routes } from '@angular/router';
+
+// Modules
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+//Components
 import { AppComponent } from './app.component';
+import { FooterComponent } from './Components/Footer/Footer.component';
+import { NavbarComponent } from './Components/Navbar/Navbar.component';
+import { HomeComponent } from './Pages/Home/Home.component';
+import { MenuComponent } from './Pages/Menu/Menu.component';
+import { AboutComponent } from './Pages/About/About.component';
+import { ContactComponent } from './Pages/contact/contact.component';
+
+// Routes
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+];
 
 @NgModule({
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    MenuComponent,
+    AboutComponent,
+    ContactComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
